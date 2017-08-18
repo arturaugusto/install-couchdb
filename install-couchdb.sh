85917c582a4a1042aa46fe44322e3618ae39cf86
Artur <arturaugusto@gmail.com>
2017-08-18 08:32:19 -0300

    Update install-couchdb.sh

--- install-couchdb.sh
+++ install-couchdb.sh
@@ -9,7 +9,7 @@ sudo apt-get --no-install-recommends -y install \
 
 wget http://ftp.unicamp.br/pub/apache/couchdb/source/2.1.0/apache-couchdb-2.1.0.tar.gz
 
-tar -xvzf apache-couchdb-2.0.0.tar.gz
+tar -xvzf apache-couchdb-2.1.0.tar.gz
 cd apache-couchdb-2.1.0/
 ./configure && make release
 
